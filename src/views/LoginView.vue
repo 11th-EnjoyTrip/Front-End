01cbeb9abb35f3f67460e1d75d243c47506072aa
yh-project <bluewhaleyh@gmail.com>
2024-05-04 16:38:22 +0900

    design: 로그인 페이지로의 진입

---
+++ src/views/LoginView.vue
@@ -0,0 +1,11 @@
+<script setup>
+import LoginLogo from "@/components/Login/LoginLogo.vue";
+</script>
+
+<template>
+    <div class="d-flex flex-column align-items-center w-100 h-auto">
+        <LoginLogo />
+    </div>
+</template>
+
+<style scoped></style>
