c6f221316ef2903797f7130cd911fbe9e8a1f281
yh-project <bluewhaleyh@gmail.com>
2024-05-08 22:13:52 +0900

    feat: 라우팅 전 사전 작업 구현 (#21)

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import HomeView from "@/views/HomeView.vue";
 import LoginView from "@/views/LoginView.vue";
 import SignUpView from "@/views/SignUpView.vue";
 import MyPageView from "@/views/MyPageView.vue";
+import { useUserInfoStore } from "@/stores/userInfo.js";
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -26,6 +27,12 @@ const router = createRouter({
             path: "/mypage",
             name: "mypage",
             component: MyPageView,
+            beforeEnter: (to, from) => {
+                const store = useUserInfoStore();
+                if (!store.getState) {
+                    return { name: "login" };
+                }
+            },
         },
         /*{
             path: "/about",
