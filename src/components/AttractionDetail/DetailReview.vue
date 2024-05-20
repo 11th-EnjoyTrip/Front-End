4dbfdc8a27d00e720570e7da7ca05ac367568fbe
yh-project <bluewhaleyh@gmail.com>
2024-05-20 23:09:48 +0900

    design: 리뷰 관련 컴포넌트 디자인 (#45)
    리뷰 조회 컴포넌트 디자인
    
    리뷰 작성 컴포넌트 디자인
    
    리뷰 수정 컴포넌트 디자인

--- src/components/AttractionDetail/DetailReview.vue
+++ src/components/AttractionDetail/DetailReview.vue
@@ -1,19 +1,53 @@
 <script setup>
+import DetailReviewAdd from "@/components/AttractionDetail/DetailReviewAdd.vue";
+import DetailReviewCard from "@/components/AttractionDetail/DetailReviewCard.vue";
+import { ref } from "vue";
+
 defineProps({
     contentId: Number,
 });
+
+const isAdding = ref(false);
+const changeState = () => (isAdding.value = !isAdding.value);
 </script>
 
 <template>
-    <div class="w-100">
-        <div class="review-title">리뷰</div>
+    <div class="w-100 mt-5 mb-5 pb-5">
+        <div class="review-title">
+            <div>리뷰</div>
+            <button v-if="!isAdding" class="review-btn" @click="changeState">리뷰 추가</button>
+        </div>
+        <div v-if="isAdding" class="w-100">
+            <DetailReviewAdd @changeState="changeState" />
+        </div>
+        <div class="w-100">
+            <DetailReviewCard v-for="(v, i) in 3" :key="i" />
+        </div>
     </div>
 </template>
 
 <style scoped>
 .review-title {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0 10px;
     font-size: 22px;
     font-weight: 600;
     color: #374553;
+}
+
+.review-btn {
+    background-color: #e1e1e1;
+    border: 0;
+    border-radius: 20px;
+    padding: 10px 20px;
+    font-size: 14px;
+    font-weight: 700;
+}
+
+.review-btn:hover {
+    background-color: #1769ff;
+    color: white;
 }
 </style>
---
+++ src/components/AttractionDetail/DetailReviewCard.vue
@@ -0,0 +1,88 @@
+<script setup>
+import IconLike from "@/components/icons/IconLike.vue";
+import { ref } from "vue";
+
+const isEditing = ref(false);
+const ment = ref(
+    "여기 여행지 진짜 너무 좋아서 미쳐 환장한다니까요 제가 미친게 아니라요 진짜라니까요 여기 여행지 진짜 너무 좋아서 미쳐 환장한다니까요 제가 미친게 아니라요 진짜라니까요 여기 여행지 진짜 너무 좋아서 미쳐 환장한다니까요 제가 미친게 아니라요 진짜라니까요 여기 여행지 진짜 너무 좋아서 미쳐 환장한다니까요 제가 미친게 아니라요 진짜라니까요 여기 여행지 진짜 너무 좋아서 미쳐 환장한다니까요 제가 미친게 아니라요 진짜라니까요 여기 여행지 진짜 너무 좋아서 미쳐 환장한다니까요 제가 미친게 아니라요 진짜라니까요 여기 여행지 진짜 너무 좋아서 미쳐 환장한다니까요 제가 미친게 아니라요 진짜라니까요 여기 여행지 진짜 너무 좋아서 미쳐 환장한다니까요 제가 미친게 아니라요 진짜라니까요 여기 여행지 진짜 너무 좋아서 미쳐 환장한다니까요 제가 미친게 아니라요 진짜라니까요"
+);
+</script>
+
+<template>
+    <div class="w-100 p-4 mt-4 review-container">
+        <div class="w-100 d-flex justify-content-between align-items-center">
+            <div class="d-flex align-items-center column-gap-3">
+                <img src="@/assets/noPicture2.png" alt="프로필 사진" class="review-card-img" />
+                <div>
+                    <div class="review-writer">최요하</div>
+                    <div class="review-reg-date">2024.05.20 18:30:00</div>
+                </div>
+            </div>
+            <IconLike :width="24" :height="24" :color="'#ff2c51'" :isLike="true" @click="isEditing = !isEditing" />
+        </div>
+        <hr />
+        <div v-if="!isEditing" class="review-content">
+            여기 여행지 진짜 너무 좋아서 미쳐 환장한다니까요? 제가 미친게 아니라요 진짜라니까요? 여기 여행지 진짜 너무
+            좋아서 미쳐 환장한다니까요? 제가 미친게 아니라요 진짜라니까요? 여기 여행지 진짜 너무 좋아서 미쳐
+            환장한다니까요? 제가 미친게 아니라요 진짜라니까요? 여기 여행지 진짜 너무 좋아서 미쳐 환장한다니까요? 제가
+            미친게 아니라요 진짜라니까요? 여기 여행지 진짜 너무 좋아서 미쳐 환장한다니까요? 제가 미친게 아니라요
+            진짜라니까요? 여기 여행지 진짜 너무 좋아서 미쳐 환장한다니까요? 제가 미친게 아니라요 진짜라니까요? 여기
+            여행지 진짜 너무 좋아서 미쳐 환장한다니까요? 제가 미친게 아니라요 진짜라니까요? 여기 여행지 진짜 너무 좋아서
+            미쳐 환장한다니까요? 제가 미친게 아니라요 진짜라니까요? 여기 여행지 진짜 너무 좋아서 미쳐 환장한다니까요?
+            제가 미친게 아니라요 진짜라니까요?
+        </div>
+        <textarea v-else class="review-edit" :value="ment"></textarea>
+    </div>
+</template>
+
+<style scoped>
+.review-container {
+    border-radius: 10px;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.05);
+}
+
+.review-card-img {
+    width: 48px;
+    height: 48px;
+    object-fit: cover;
+    border-radius: 32px;
+}
+
+.review-writer {
+    font-size: 18px;
+    font-weight: 600;
+    color: #374553;
+}
+
+.review-reg-date {
+    font-size: 14px;
+    color: #646f7c;
+}
+
+.review-content {
+    font-size: 16px;
+    font-weight: 500;
+    color: #374553;
+    padding-left: 10px;
+    padding-right: 10px;
+}
+
+.review-edit {
+    width: 100%;
+    height: 300px;
+    font-size: 16px;
+    font-weight: 500;
+    color: #374553;
+    border-radius: 10px;
+    padding: 10px 10px;
+    resize: none;
+}
+
+.review-edit:focus {
+    outline: none;
+}
+
+.review-edit::-webkit-scrollbar {
+    width: 0;
+}
+</style>
