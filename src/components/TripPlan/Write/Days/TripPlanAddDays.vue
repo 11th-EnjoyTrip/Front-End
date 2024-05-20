12b654a963544236838a605b411537e9822419a6
bluewhaleyh <bluewhaleyh@gmail.com>
2024-05-20 15:45:36 +0900

    design: 여행 계획 수정 컴포넌트 디자인 (#43)
    여행 계획 추가 컴포넌트에서 type props 를 이용해서 판단

--- src/components/TripPlan/Write/Days/TripPlanAddDays.vue
+++ src/components/TripPlan/Write/Days/TripPlanAddDays.vue
@@ -8,12 +8,12 @@ import TripPlanDaysList from "@/components/TripPlan/Days/TripPlanDaysList.vue";
         <div v-for="(v, i) in 3" :key="i">
             <div class="mt-5 text-center fw-bold fs-5">Day {{ i + 1 }}</div>
             <div v-if="i % 2 == 0" class="d-flex">
-                <TripPlanDaysList :day="i" :type="'edit'" />
+                <TripPlanDaysList :day="i" :type="'add'" />
                 <TripPlanDaysMap :day="i" />
             </div>
             <div v-else class="d-flex">
                 <TripPlanDaysMap :day="i" />
-                <TripPlanDaysList :day="i" :type="'edit'" />
+                <TripPlanDaysList :day="i" :type="'add'" />
             </div>
         </div>
     </div>
