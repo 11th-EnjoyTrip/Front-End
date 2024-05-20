12836ca49c0f665dcc1591e32a391eacd0d3eaff
bluewhaleyh <bluewhaleyh@gmail.com>
2024-05-20 17:40:32 +0900

    design: 리뷰 등록 컴포넌트 디자인 (#45)
    임시 저장

---
+++ src/components/AttractionDetail/DetailReview.vue
@@ -0,0 +1,19 @@
+<script setup>
+defineProps({
+    contentId: Number,
+});
+</script>
+
+<template>
+    <div class="w-100">
+        <div class="review-title">리뷰</div>
+    </div>
+</template>
+
+<style scoped>
+.review-title {
+    font-size: 22px;
+    font-weight: 600;
+    color: #374553;
+}
+</style>
--- src/views/AttractionDetailView.vue
+++ src/views/AttractionDetailView.vue
@@ -1,45 +1,57 @@
 <script setup>
 import NavComp from "@/components/Nav/NavComp.vue";
-import DetailContents from '@/components/AttractionDetail/DetailContents.vue';
-import DetailMap from '@/components/AttractionDetail/DetailMap.vue';
-import { useRouter } from 'vue-router';
+import DetailContents from "@/components/AttractionDetail/DetailContents.vue";
+import DetailMap from "@/components/AttractionDetail/DetailMap.vue";
+import DetailReview from "@/components/AttractionDetail/DetailReview.vue";
+import { useRouter } from "vue-router";
 import { onMounted } from "vue";
 import { attractionDetail } from "@/apis/attractionApi";
-import {ref, computed} from "vue"
+import { ref, computed } from "vue";
 
 const router = useRouter();
 const content = ref(null);
 
 onMounted(async () => {
-  await attractionDetail(router.currentRoute.value.params.id)
-    .then(response => {
-      console.log(response.data);
-      content.value = {...response.data}
-    }).catch(error => console.log(error));
-})
+    await attractionDetail(router.currentRoute.value.params.id)
+        .then((response) => {
+            console.log(response.data);
+            content.value = { ...response.data };
+        })
+        .catch((error) => console.log(error));
+});
 
 const getContent = computed(() => {
-  console.log(1211)
-  return content.value;
-})
-
+    console.log(1211);
+    return content.value;
+});
 </script>
 
 <template>
-  <div>
-    <NavComp :withLower="true" />
-    <div v-if="content" class="mx-auto m-5 attraction-detail-view">
-      <div class="card g-4" style="box-sizing: border-box; border-radius: 25px; border:0;">
-        <img v-if="content.firstImage" :src="content.firstImage" style="border-radius: 20px;border: 1px solid rgba(0, 0, 0, 0.1);">
-        <img v-else src="@/assets/noPicture2.png" width="100%" height="40%" style="border-radius: 20px;border: 1px solid rgba(0, 0, 0, 0.1);">
-      </div>
-      <DetailContents :content="getContent"/>
-      <DetailMap :content="getContent"/>
+    <div>
+        <NavComp :withLower="true" />
+        <div v-if="content" class="mx-auto m-5 attraction-detail-view">
+            <div class="card g-4" style="box-sizing: border-box; border-radius: 25px; border: 0">
+                <img
+                    v-if="content.firstImage"
+                    :src="content.firstImage"
+                    style="border-radius: 20px; border: 1px solid rgba(0, 0, 0, 0.1)"
+                />
+                <img
+                    v-else
+                    src="@/assets/noPicture2.png"
+                    width="100%"
+                    height="40%"
+                    style="border-radius: 20px; border: 1px solid rgba(0, 0, 0, 0.1)"
+                />
+            </div>
+            <DetailContents :content="getContent" />
+            <DetailMap :content="getContent" />
+            <DetailReview :contentId="1" />
+        </div>
     </div>
-  </div>
 </template>
 <style scoped>
 .attraction-detail-view {
-  width: 65% !important;
+    width: 65% !important;
 }
 </style>
