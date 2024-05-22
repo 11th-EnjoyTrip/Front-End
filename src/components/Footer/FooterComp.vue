cacaa9796302af567e6fb5bf4f22313356cf464f
nkyungm <skarudals27@naver.com>
2024-05-22 18:01:36 +0900

    feat : footer 개발 (#57)
    - footer 구현(미완료)

--- src/components/Footer/FooterComp.vue
+++ src/components/Footer/FooterComp.vue
@@ -1,10 +1,46 @@
 <script>
 </script>
 <template>
-    <div style="background-color: #EEEEEE;">
-        
-    </div>
+    <div class="mx-auto" style="background-color: #EEEEEE; max-width: 1200px; ">
+    <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top p-3 pb-4">
+        <div class="col-md-6 text-body-secondary">
+            <div class="d-flex">
+                <img class="" src="@/assets/travelog_logo.png" width="60px" height="55px"/>
+                <h3 class="ps-2 pt-4">Travelog</h3>
+            </div>
+            <p class="footer-first">대표 : 구미 5반 김정인, 최요하, 남경민</p>
+            <p class="footer-first">사업자번호 : 123-4567-987</p>
+            <p class="footer-first">주소: 302, 3 Industrial Complex 3, Gumi-si, Gyeongsangbuk-do, Republic of Korea</p>
+            <p class="mb-0" style="font-size: 50%;">&copy; SSAFY Gumi 5. Travelog</p>
+        </div>
+
+        <div class="col-md-4">
+            <ul class="nav justify-content-end">
+                <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary footer-second">이용약관</a></li>
+                <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary footer-second">개인정보처리방침</a></li>
+                <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary footer-second">공지사항</a></li>
+                <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary pt-0 pb-0" style="font-size: 70%;">문의하기</a></li>
+            </ul>
+            <div class="d-flex justify-content-end" >
+                <img src="@/assets/trip/ssafy.png" height="40" style="margin-top: 15px;" />
+                <img src="@/assets/trip/tourapi.png" height="25" style="margin-top: 20px;" />
+                <img src="@/assets/trip/korea_tourism_organization.png" style="margin-top: 7px;"/>
+                <img src="@/assets/trip/sports_and_tourism.png" style="margin-right: 0px !important; padding-right: 0px !important;" />
+            </div>
+        </div>
+
+    </footer>
+</div>
 </template>
 <style scoped>
-
+.footer-first{
+    margin-bottom: 0px;
+    font-size: 70%;
+}
+.footer-second{
+    font-size: 70%;
+    border-right: 1px solid #666666 !important;
+    padding-top: 0px;
+    padding-bottom: 0px;
+}
 </style>
--- src/views/AttractionView.vue
+++ src/views/AttractionView.vue
@@ -1,13 +1,9 @@
-git fetch origin
-git checkout feat-#52-chatgpt
-
-
 <script setup>
 import SearchBox from "@/components/Attraction/SearchBox.vue";
 import NavComp from "@/components/Nav/NavComp.vue";
 import CommonKakaoMap from "@/components/common/CommonKakaoMap.vue";
 import SearchItemCard from "@/components/Attraction/SearchItemCard.vue";
-import FooterComp from "@/components/Footer/FooterComp.vue";
+
 import { ref,onMounted } from 'vue';
 import { attractionList } from "@/apis/attractionApi";
 import InfiniteLoading from "v3-infinite-loading";
@@ -25,7 +21,7 @@ const searchItem = async(...args) => {
   keyword.value = args[3].value;
 
   // args(sido,categorys,keyword)로 서버에서 데이터 불러옴
-  page = ref(0);
+  page.value = 0;
   await loadAttractionList();
 };
 
@@ -81,7 +77,7 @@ onMounted(() => {
       </template>
     </InfiniteLoading>
     </div>
-    <FooterComp/>
+    
   </div>
 </template>
 
